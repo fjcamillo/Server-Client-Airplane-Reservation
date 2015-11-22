a0fe3414bbd1f1c956d97278a50fc33bb377d36b
Jerhone <fjcamillo.dev@gmail.com>
2015-11-22 11:36:03 +0800

    User and Password
    Added the username and the password of the user

--- AirplaneServer.py
+++ AirplaneServer.py
@@ -1,1 +1,38 @@
+import threading
+import socket
+import time
+import os
+
 __author__ = 'Francisc'
+
+class AirplaneTypes:
+
+    def __init__(self):
+        pass
+
+    def boeing(self):
+        pass
+
+    def airbus(self):
+        pass
+
+    def new_airplane_brand(self):
+        pass
+
+def server_user():
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    username = raw_input("                                    "
+                         "             Representative's Name: ")
+    user_password = raw_input("                                    "
+                              "             Representative's Password: ")
+
+
+def main():
+    os.system('cls')
+    server_user()
+    #print "<<<<Welcome to the Airplane Reservation Server>>>>\n"
+    #print "Representative Name: "
+
+if __name__ == '__main__':
+    main()
+
